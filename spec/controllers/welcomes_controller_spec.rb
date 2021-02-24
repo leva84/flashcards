704738a7e23baf32802cface799f138b7b676e69
leva84 <denfrut8@gmail.com>
2021-02-24 18:37:37 +0300

    Added rubocop

--- spec/controllers/welcomes_controller_spec.rb
+++ spec/controllers/welcomes_controller_spec.rb
@@ -1,9 +1,9 @@
-require  'rails_helper'
-RSpec.describe WelcomesController, type:  :controller  do
-  # группа тестов, которая отвечает за тестирование экшена  index
-  describe "GET #index" do
-    # тест проверяет что возвращается корретный запрос
-    it "returns a success response" do
+# frozen_string_literal: true
+
+require 'rails_helper'
+RSpec.describe WelcomesController, type: :controller do
+  describe 'GET #index' do
+    it 'returns a success response' do
       get :index
       expect(response).to have_http_status(:ok)
     end
