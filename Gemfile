6a1f1b308dacabfcd641890827d4561f3d40049a
leva84 <denfrut8@gmail.com>
2021-03-01 23:46:17 +0300

    Added factory_bot

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'webpacker', '~> 4.0'
 
 group :development, :test do
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
+  gem 'factory_bot_rails'
   gem 'rspec-rails', '~> 4.0.2'
 end
 
--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -3,46 +3,39 @@
 require 'rails_helper'
 
 RSpec.describe Card, type: :model do
-  text = (0..5001).to_a.map(&:to_s).join
+  context 'checking creation and validations' do
+    let!(:card1) { create(:card) }
+    text = (0..5001).to_a.map(&:to_s).join
 
-  fixtures :cards
-  it 'fixture method defined one' do
-    cards(:one)
+    it 'has one after adding one' do
+      expect(Card.count).to eq 1
+      expect(card1.original_text == 'FBI and CIA')
+    end
+
+    it 'create without original_text Card' do
+      expect(build(:card, original_text: nil).save).to eq false
+    end
+
+    it 'check original_text < 5 characters' do
+      expect(build(:card, original_text: 'FBI').save).to eq false
+    end
+
+    it 'check original_text > 5000 characters' do
+      expect(build(:card, original_text: text).save).to eq false
+    end
+
+    it 'create without translated_text Card' do
+      expect(build(:card, original_text: 'FBI and CIA', translated_text: nil).save).to eq true
+    end
+
+    it 'check translated_text < 5 characters' do
+      expect(build(:card, original_text: 'FBI and CIA', translated_text: 'CIA').save).to eq false
+    end
+
+    it 'check translated_text > 5000 characters' do
+      expect(build(:card, original_text: 'FBI and CIA', translated_text: text).save).to eq false
+    end
+
+    after(:all) { Card.destroy_all }
   end
-
-  it 'has one after adding one' do
-    expect(Card.count).to eq 1
-  end
-
-  it 'create without original_text Card' do
-    card = Card.new(original_text: nil)
-    expect(card.save).to eq false
-  end
-
-  it 'check original_text < 5 characters' do
-    card = Card.new(original_text: 'FBI')
-    expect(card.save).to eq false
-  end
-
-  it 'check original_text > 5000 characters' do
-    card = Card.new(original_text: text)
-    expect(card.save).to eq false
-  end
-
-  it 'create without translated_text Card' do
-    card = Card.new(original_text: 'FBI and CIA', translated_text: nil)
-    expect(card.save).to eq true
-  end
-
-  it 'check translated_text < 5 characters' do
-    card = Card.new(original_text: 'FBI and CIA', translated_text: 'CIA')
-    expect(card.save).to eq false
-  end
-
-  it 'check translated_text > 5000 characters' do
-    card = Card.new(original_text: 'FBI and CIA', translated_text: text)
-    expect(card.save).to eq false
-  end
-
-  after(:all) { Card.destroy_all }
 end
